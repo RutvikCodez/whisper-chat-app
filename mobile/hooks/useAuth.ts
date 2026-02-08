7c0b7a73c560129922f3d74a3bf4464a8bb618a2
RutvikCodez <rutvikdarji21@gnu.ac.in>
2026-02-08 13:53:07 +0530

    Update useAuth - the method where we are calling api becuase we made chnages in axios.ts file

--- mobile/hooks/useAuth.ts
+++ mobile/hooks/useAuth.ts
@@ -1,12 +1,15 @@
-import { useApi } from "@/lib/axios"
-import { useMutation } from "@tanstack/react-query"
+import { useApi } from "@/lib/axios";
+import { useMutation } from "@tanstack/react-query";
 
 export const useAuthCallback = () => {
-    const api = useApi()
-    return useMutation({
-        mutationFn: async() => {
-            const {data} = await api.post("/auth/callback")
-            return data
-        }
-    })
-}
+  const { apiWithAuth } = useApi();
+  return useMutation({
+    mutationFn: async () => {
+      const { data } = await apiWithAuth<User>({
+        method: "POST",
+        url: "/auth/callback",
+      });
+      return data;
+    },
+  });
+};
